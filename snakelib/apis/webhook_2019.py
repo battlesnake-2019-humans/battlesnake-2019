12d46025b075deb9dcf5fc5e80c9a0c6752f8e6b
Charlie Friend <charles.d.friend@gmail.com>
2019-01-09 20:09:31 -0800

    Use explicit from_json in webhook API

--- snakelib/apis/webhook_2019.py
+++ snakelib/apis/webhook_2019.py
@@ -4,34 +4,45 @@ Battlesnake Webhook API Implementation
 
 An implementation of all models in the 'Snake' webhook API. Response objects
 are implemented as superclasses of dict for easy conversion to JSON.
-
-Example usage:
-> def move(request_json):
->     req = SnakeRequest(request_json)
->
->     # Do some stuff with your request object...
->
->     return MoveResponse("left")
 """
+
+from typing import List
 
 
 class Coords:
-    def __init__(self, json):
-        self.x = json.get('x')
-        self.y = json.get('y')
+    def __init__(self, **kwargs):
+        self.x: int = kwargs.get('x')
+        self.y: int = kwargs.get('y')
+
+    @staticmethod
+    def from_json(json_dict):
+        c = Coords()
+        c.x = json_dict["x"]
+        c.y = json_dict["y"]
+        return c
 
     def __getitem__(self, i):
         return [self.x, self.y][i]
 
 
 class Snake:
-    def __init__(self, json):
-        self.id = json.get('id')
-        self.name = json.get('name')
-        self.health = json.get('health')
+    def __init__(self, **kwargs):
+        self.id:     str = kwargs.get("id")
+        self.name:   str = kwargs.get("name")
+        self.health: int = kwargs.get("health")
+        self.body:   List[Coords] = kwargs.get("body")
 
-        if type(json.get('body')) == list:
-            self.body = [Coords(x) for x in json['body']]
+    @staticmethod
+    def from_json(json_dict):
+        s = Snake()
+        s.id = json_dict.get('id')
+        s.name = json_dict.get('name')
+        s.health = json_dict.get('health')
+
+        if type(json_dict.get('body')) == list:
+            s.body = [Coords.from_json(x) for x in json_dict['body']]
+
+        return s
 
     def head(self):
         if getattr(self, 'body'):
@@ -39,48 +50,69 @@ class Snake:
 
 
 class Board:
-    def __init__(self, json):
-        self.height = json.get('height')
-        self.width = json.get('width')
+    def __init__(self, **kwargs):
+        self.width:  int = kwargs.get("width")
+        self.height: int = kwargs.get("height")
+        self.food:   List[Coords] = kwargs.get("food")
+        self.snakes: List[Snake] = kwargs.get("snakes")
 
-        if type(json.get('food')) == list:
-            self.food = [Coords(x) for x in json['food']]
+    @staticmethod
+    def from_json(json_dict):
+        b = Board()
+        b.height = json_dict.get('height')
+        b.width = json_dict.get('width')
 
-        if type(json.get('snakes')) == list:
-            self.snakes = [Snake(x) for x in json['snakes']]
+        if type(json_dict.get('food')) == list:
+            b.food = [Coords.from_json(x) for x in json_dict['food']]
+
+        if type(json_dict.get('snakes')) == list:
+            b.snakes = [Snake.from_json(x) for x in json_dict['snakes']]
+
+        return b
 
 
 class Game:
-    def __init__(self, json):
-        self.id = json.get('id')
+    def __init__(self, **kwargs):
+        self.id: str = kwargs.get("id")
+
+    @staticmethod
+    def from_json(json_dict):
+        g = Game()
+        g.id = json_dict.get('id')
+        return g
 
 
 class SnakeRequest:
-    def __init__(self, json):
-        self.turn = json.get('turn')
+    def __init__(self, **kwargs):
+        self.turn:  int = kwargs.get("turn")
+        self.game:  Game = kwargs.get("game")
+        self.board: Board = kwargs.get("board")
+        self.you:   Snake = kwargs.get("you")
 
-        if type(json.get('game')) == dict:
-            self.game = Game(json['game'])
+    @staticmethod
+    def from_json(json_dict):
+        req = SnakeRequest()
+        req.turn = json_dict.get('turn')
 
-        if type(json.get('board')) == dict:
-            self.board = Board(json['board'])
+        if type(json_dict.get('game')) == dict:
+            req.game = Game.from_json(json_dict['game'])
 
-        if type(json.get('you')) == dict:
-            self.you = Snake(json['you'])
+        if type(json_dict.get('board')) == dict:
+            req.board = Board.from_json(json_dict['board'])
+
+        if type(json_dict.get('you')) == dict:
+            req.you = Snake.from_json(json_dict['you'])
 
 
-class MoveResponse(dict):
+class MoveResponse:
     def __init__(self, move):
-        super(MoveResponse, self).__init__()
+        self.move: str = move
 
         assert move in ['up', 'down', 'left', 'right'], \
             "Move must be one of [up, down, left, right]"
-        self["move"] = move
 
 
-class StartResponse(dict):
+class StartResponse:
     def __init__(self, color):
-        super(StartResponse, self).__init__()
-
+        self.color: str = color
         assert type(color) is str, "Color value must be string"
-        self["color"] = color
