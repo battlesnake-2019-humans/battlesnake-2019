0bafb16ec798b48902ee65c3c6453fedc2776d05
Charlie Friend <charles.d.friend@gmail.com>
2019-01-13 14:41:52 -0800

    Add sendstate command

--- bsdebug/__main__.py
+++ bsdebug/__main__.py
@@ -1,11 +1,14 @@
 import sys
 
 from .showboard import showboard
+from .sendstate import sendstate
 
 
-cmd_string = sys.argv[1] if len(sys.argv) > 1 else "showboard"
+cmd_string = sys.argv[1]
 
 if cmd_string == "showboard":
-    showboard(sys.argv[1:])
+    showboard(*sys.argv[1:])
+elif cmd_string == "sendstate":
+    sendstate(*sys.argv[1:])
 else:
     print("Unrecognized command")
