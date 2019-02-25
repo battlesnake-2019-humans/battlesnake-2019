1a4f42c8cae7558e413b0db87f5e6cd3d2151e98
Charlie Friend <charles.d.friend@gmail.com>
2019-02-24 21:22:32 -0800

    Initial commit of twistedsister module

---
+++ twistedsister/snake.py
@@ -0,0 +1,25 @@
+from bottle import default_app, request
+from snakelib.gamestate import *
+
+application = default_app()
+
+
+@application.post("/start")
+def start():
+    state = GameState.from_snake_request(request.json)
+    return make_start_response(color="#FF0000")
+
+
+@application.post("/move")
+def move():
+    state = GameState.from_snake_request(request.json)
+    return make_move_response(move="left")
+
+
+@application.post("/end")
+def end():
+    pass
+
+
+if __name__ == "__main__":
+    application.run(port=8080)
